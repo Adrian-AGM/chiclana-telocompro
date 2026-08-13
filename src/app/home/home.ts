5726f43d2d67e48819f2134119a3f160e779179f
AdrianAGM <adriangm17@msn.com>
2026-08-13 18:05:42 +0200

    Añadido stepper y precio

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,25 +1,37 @@
 import { Component } from '@angular/core';
-import {TuiTiles, TuiFluidTypography} from '@taiga-ui/kit';
-import { TuiIcon, TuiInputDirective, TuiTextfield } from '@taiga-ui/core';
+import { TuiTiles, TuiFluidTypography, TuiStepper, TuiConnected } from '@taiga-ui/kit';
+import { TuiIcon, TuiInputDirective, TuiTextfield, TuiNumberFormat, TuiNumberFormatSettings } from '@taiga-ui/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { TuiCurrencyPipe } from '@taiga-ui/addon-commerce';
+import { TuiInputNumber } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TuiTiles, TuiIcon, TuiInputDirective, TuiTextfield, TuiFluidTypography, FormsModule, CommonModule],
+  imports: [
+    TuiTiles,
+    TuiIcon,
+    TuiInputDirective, TuiStepper, TuiConnected, TuiNumberFormat, TuiTextfield, TuiFluidTypography, FormsModule, CommonModule, TuiCurrencyPipe, TuiInputNumber],
   templateUrl: './home.html',
   styleUrls: ['./home.less'],
 })
-export class Home {protected items = [
-        {owner: 'Pep', product: null, image: null},
-        {owner: 'Pui', product: null, image: null},
-        {owner: 'Javi', product: null, image: null},
-    ];
- 
-    protected order = new Map();
 
-    // Escucha el evento 'paste' en la ventana del navegador
+
+export class Home {
+  protected numberFormat: Partial<TuiNumberFormatSettings> = {
+    decimalSeparator: ',',
+    thousandSeparator: '.',
+  };
+  protected items = [
+    { owner: 'Pep', product: null, image: null, price: null },
+    { owner: 'Pui', product: null, image: null, price: null },
+    { owner: 'Javi', product: null, image: null, price: null },
+  ];
+
+  protected order = new Map();
+
+  // Escucha el evento 'paste' en la ventana del navegador
   onPaste(event: ClipboardEvent, currentItemIndex: number): void {
     const items = event.clipboardData?.items;
 
@@ -30,7 +42,7 @@ export class Home {protected items = [
       if (items[i].type.indexOf('image') !== -1) {
         // Convierte el elemento en un archivo (File blob)
         const file = items[i].getAsFile();
-        
+
         if (file) {
           this.convertToBase64(file, currentItemIndex);
         }
@@ -42,7 +54,7 @@ export class Home {protected items = [
   // Convierte el archivo binario en una cadena Base64 legible por la etiqueta <img>
   private convertToBase64(file: File, currentItemIndex: number): void {
     const reader = new FileReader();
-    
+
     reader.onload = (e: any) => {
       this.items[currentItemIndex].image = e.target.result;
     };
