d88fd32779712d013656ab462d1f8821ecc61408
AdrianAGM <adriangm17@msn.com>
2026-08-22 18:00:36 +0200

    Pegado de direccion de imagen con zona activa

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,13 +1,20 @@
-import { Component, signal } from '@angular/core';
-import { TuiTiles, TuiFluidTypography, TuiStepper, TuiBadge, TuiStatus } from '@taiga-ui/kit';
-import { TuiIcon, TuiInputDirective, TuiTextfield, TuiNumberFormat, TuiNumberFormatSettings, tuiTextfieldOptionsProvider, TuiButton, TuiLoader, tuiLoaderOptionsProvider } from '@taiga-ui/core';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TuiCurrencyPipe } from '@taiga-ui/addon-commerce';
-import { TuiInputNumber } from '@taiga-ui/kit';
+import { TuiActiveZone } from '@taiga-ui/cdk/directives/active-zone';
+import { TuiIcon, TuiInputDirective, TuiLoader, tuiLoaderOptionsProvider, TuiNumberFormat, TuiNumberFormatSettings, TuiTextfield, tuiTextfieldOptionsProvider } from '@taiga-ui/core';
+import { TuiFluidTypography, TuiInputNumber, TuiStepper, TuiTiles } from '@taiga-ui/kit';
 
 interface Product {
-  owner: string, baseColor: string, accentColor: string, product: string | null, loadingImage: boolean, image: string | null, price: string | null
+  owner: string,
+  baseColor: string,
+  accentColor: string,
+  product: string | null,
+  loadingImage: boolean,
+  image: string | null,
+  copiadoActivo: boolean,
+  price: string | null
 }
 
 @Component({
@@ -17,6 +24,7 @@ interface Product {
     TuiTiles,
     TuiIcon,
     TuiInputDirective,
+    TuiActiveZone,
     TuiLoader, TuiStepper, TuiNumberFormat, TuiTextfield, TuiFluidTypography, FormsModule, CommonModule, TuiCurrencyPipe, TuiInputNumber],
   templateUrl: './home.html',
   styleUrls: ['./home.less'],
@@ -34,9 +42,9 @@ export class Home {
     thousandSeparator: '.',
   };
   protected items: Product[] = [
-    { owner: 'PEP', baseColor: '#FF9966', accentColor: '#FFF500', product: null, loadingImage: false, image: null, price: null },
-    { owner: 'PUI', baseColor: '#5AA7F7', accentColor: '#FF9966', product: null, loadingImage: false, image: null, price: null },
-    { owner: 'JAVIER', baseColor: '#FFF500', accentColor: '#5AA7F7', product: null, loadingImage: false, image: null, price: null },
+    { owner: 'PEP', baseColor: '#FF9966', accentColor: '#FFF500', product: null, copiadoActivo: false, loadingImage: false, image: null, price: null },
+    { owner: 'PUI', baseColor: '#5AA7F7', accentColor: '#FF9966', product: null, copiadoActivo: false, loadingImage: false, image: null, price: null },
+    { owner: 'JAVIER', baseColor: '#FFF500', accentColor: '#5AA7F7', product: null, copiadoActivo: false, loadingImage: false, image: null, price: null },
   ];
 
   protected order = new Map();
@@ -44,44 +52,33 @@ export class Home {
   // Escucha el evento 'paste' en la ventana del navegador
   onPaste(event: ClipboardEvent, currentItemIndex: number): void {
     this.items[currentItemIndex].loadingImage = true;
-    const items = event.clipboardData?.items;
-
-    if (!items) {
+    const clipboardData = event.clipboardData;
+    if (!clipboardData) {
       this.items[currentItemIndex].loadingImage = false;
       return;
     }
-    let found = false;
-    // Busca si hay alguna imagen en los elementos del portapapeles
-    for (let i = 0; i < items.length; i++) {
-      if (items[i].type.indexOf('image') !== -1) {
-        // Convierte el elemento en un archivo (File blob)
-        const file = items[i].getAsFile();
 
-        if (file) {
-          found = true;
-          this.convertToBase64(file, currentItemIndex);
-        }
-        break; // Detiene el bucle tras encontrar la primera imagen
-      }
-    }
-    if (!found) {
+    // 1. Intentar leer si lo que se pegó es un texto/URL
+    const pastedText = clipboardData.getData('text');
+    if (pastedText && this.items[currentItemIndex].image !== pastedText && this.esUrlValida(pastedText)) {
+      this.items[currentItemIndex].image = pastedText;
+      return;
+    } else {
       this.items[currentItemIndex].loadingImage = false;
     }
   }
 
-
-  // Convierte el archivo binario en una cadena Base64 legible por la etiqueta <img>
-  private convertToBase64(file: File, currentItemIndex: number): void {
-    const reader = new FileReader();
-
-    reader.onload = (e: any) => {
-      this.items[currentItemIndex].image = e.target.result;
-    };
-
-    reader.readAsDataURL(file);
+  // Validación básica para comprobar si el texto tiene formato de URL
+  private esUrlValida(texto: string): boolean {
+    return texto.startsWith('http://') || texto.startsWith('https://');
   }
 
   onImagenCargada(currentItemIndex: number) {
     this.items[currentItemIndex].loadingImage = false;
+  }
+
+  protected onParentActiveZone(active: boolean, currentItemIndex: number): void {
+    console.log(active);
+    this.items[currentItemIndex].copiadoActivo = active;
   }
 }
